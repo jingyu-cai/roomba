76223b8111895594d6b7816f117f974f17ca7949
Jingyu Cai <caijingyududu@gmail.com>
2021-05-23 23:40:47 +0800

    changed action and states

--- scripts/action_states/generate_action_states.py
+++ scripts/action_states/generate_action_states.py
@@ -5,12 +5,12 @@ import os
 from numpy.lib.function_base import diff
 
 
-# Initialize how many bins, dumbbells, balls, and obstacles there are, and
+# Initialize how many nodes, dumbbells, balls, and obstacles there are, and
 #   right now we are assuming that there will be exactly 1 colored object of
 #   its kind, so there won't be 2 red dumbbells
-# Note: index 0 = red, index 1 = blue, so BINS means there is 1 red bin 
-#   and 1 blue bin
-BINS = [1, 1]
+# Note: except for NODES, index 0 = red, index 1 = blue, so BALLS means there is 
+#   1 red ball and 1 blue ball
+NODES = [0, 1, 2, 3, 4, 5, 6]
 DUMBBELLS = [1, 1]
 # BALLS = [1, 1]
 OBSTACLES = [1, 1]
@@ -27,7 +27,7 @@ def append_object_states(all_lists, objects):
             all_lists.append([0, i + 1])
 
 
-def generate_states(bins: list, dumbbells: list, obstacles: list):
+def generate_states(nodes: list, dumbbells: list, obstacles: list):
     """ This generates a matrix of states based on the distribution of objects """
 
     # Note: 0 = starting point/origin, 1 = red bin, 2 = blue bin
@@ -37,10 +37,8 @@ def generate_states(bins: list, dumbbells: list, obstacles: list):
     #   ball states for each ball, obstacle states for each obstacle, in that order
     all_lists = []
 
-    # Calculate the robot states based on origin + number of bins and append
-    robot_states = [(i + 1) for i in range(len(bins))]
-    robot_states.insert(0, 0)
-    all_lists.append(robot_states)
+    # Append nodes, which are robot states, to the all_lists
+    all_lists.append(nodes)
 
     # Calculate the dumbbell, ball, and obstacle states and append
     append_object_states(all_lists, dumbbells)
@@ -52,12 +50,12 @@ def generate_states(bins: list, dumbbells: list, obstacles: list):
     return states
 
 
-def generate_actions(bins: list, dumbbells: list, obstacles: list):
+def generate_actions(dumbbells: list, obstacles: list):
     """ This generates a matrix of actions based on the distribution of objects, and
     we always assume that the correct object would move to the correctly-colored bin """
 
     # Note: for column 1, 0 = dumbbell, 1 = ball, 2 = obstacle; for column 2,
-    #   1 = red, 2 = blue; for column 3, 1 = red bin, 12= blue bin
+    #   1 = red, 2 = blue; for column 3, 6 = red bin, 0 = blue bin
 
     # Create a list to save all the actions below for permutation calculation,
     #   this list would contain the types of objects, the colors of the objects,
@@ -67,11 +65,11 @@ def generate_actions(bins: list, dumbbells: list, obstacles: list):
     # Append the types of objects, which are pre-determined
     all_lists.append([0, 1])
 
-    # Find the minimum number of colors between bins, dumbbells, balls, and
-    #   obstacles to ensure that we won't have an addition color that cannot
-    #   be sorted (we can assume this since the colors will always be created
-    #   in a red -> blue -> sth else order)
-    num_of_colors = min(len(bins), len(dumbbells), len(obstacles))
+    # Find the minimum number of colors between dumbbells, balls, and obstacles
+    #   to ensure that we won't have an addition color that cannot be sorted
+    #   (we can assume this since the colors will always be created in a red ->
+    #   blue -> sth else order)
+    num_of_colors = min(len(dumbbells), len(obstacles))
 
     # Append the types of object colors for the objects
     all_lists.append(np.arange(1, num_of_colors + 1))
@@ -79,12 +77,20 @@ def generate_actions(bins: list, dumbbells: list, obstacles: list):
     # We run a permutations algorithm on all_lists
     temp_actions = list(itertools.product(*all_lists))
 
-    # Finally, we add a third column for bins, which are determined by the second
+    # Finally, we add a third column for nodes, which are determined by the second
     #   column of the color of the object
     actions = []
     for action in temp_actions:
         action = list(action)
-        action.append(action[1])
+
+        # If the color is red, it goes to the red bin (node 6)
+        if action[len(action) - 1] == 1:
+            action.append(6)
+
+        # If the color is blue, it goes to the red bin (node 0)
+        elif action[len(action) - 1] == 2:
+            action.append(0)
+
         actions.append(action)
 
     return actions
@@ -165,11 +171,11 @@ def save_data(data, name):
 if __name__=="__main__":
 
     # Create and save the state space
-    states = generate_states(BINS, DUMBBELLS, OBSTACLES)
+    states = generate_states(NODES, DUMBBELLS, OBSTACLES)
     save_data(states, "states.csv")
 
     # Create and save the action space
-    actions = generate_actions(BINS, DUMBBELLS, OBSTACLES)
+    actions = generate_actions(DUMBBELLS, OBSTACLES)
     save_data(actions, "actions.csv")
 
     # Create and save the action matrix
