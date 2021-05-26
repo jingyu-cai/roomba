cec5f4da44259ac7669a5e6fb6ae8e62cc220245
Yoon Jeong <yoonsjeong@cs.uchicago.edu>
2021-05-26 13:09:31 -0500

    added line to roomlaunch and finer tuned kinematics

--- scripts/kinematics.py
+++ scripts/kinematics.py
@@ -19,44 +19,77 @@ class RobotMovement(object):
         self.move_arm  = moveit_commander.MoveGroupCommander("arm")
         self.move_grip = moveit_commander.MoveGroupCommander("gripper")
         # init movement
+        self.twist = Twist()
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=100)
         rospy.init_node('turtlebot3_movement')
 
-    def lower_arm(self):
-        lower_pos = [0, .85, -.3, -.35]
-
-        self.move_arm.go(lower_pos, wait=True)
-        self.move_arm.stop()
-    def raise_arm(self):
-        arm = {
-            "upper": [0.3, 0.5, -0.3, -0.5],
-            "upper_diag": [0.65, 0.25, -0.3, -0.5],
-            "side": [1.5, 0.25, -0.3, -0.5],
-        }
-
-        self.move_arm.go(arm["upper"], wait=True)
-        self.move_arm.stop()
-        self.move_arm.go(arm["upper_diag"], wait=True)
-        self.move_arm.stop()
-        self.move_arm.go(arm["side"], wait=True)
-        self.move_arm.stop()
-
+    # GRANULAR GRIP MOVEMENT FUNCS
     def open_grip(self):
         open_grip = [0.016, 0.016]
         self.move_grip.go(open_grip)
+    def close_grip(self):
+        close_grip = [0.010, 0.010]
+        self.move_grip.go(close_grip)
 
+    # GRANULAR ARM MOVEMENT FUNCS
+    def lower_arm(self):
+        lower_pos = [0, 0.7, 0, -0.65]
+        self.move_arm.go(lower_pos, wait=True)
+    def upper_arm(self):
+        upper_pos = [0, 0.05, -0.5, -0.65]
+        self.move_arm.go(upper_pos, wait=True)
+    def angle_arm(self):
+        angle_pos = [0.75, 0.05, -0.5, -0.65]
+        self.move_arm.go(angle_pos, wait=True)
+
+    # GRANULAR TWIST MOVEMENT FUNCS
+    def stop(self):
+        self.twist.linear.x = 0
+        self.twist.angular.z = 0
+        self.cmd_vel_pub.publish(self.twist)
+    def move_forward(self, secs=3):
+        self.twist.linear.x = 0.1
+        self.cmd_vel_pub.publish(self.twist)
+        time.sleep(secs)
+        self.stop()
+    def move_back(self, secs=3):
+        self.twist.linear.x = -0.1
+        self.cmd_vel_pub.publish(self.twist)
+        time.sleep(secs)
+        self.stop()
+
+    # COMPOUND MOVEMENTS
+    def pick_up(self):
+        """ Picks up dumbbell and angles out of camera POV
+        """
+        self.lower_arm()
+        self.close_grip()
+        self.upper_arm()
+        self.angle_arm()
+    def let_go(self):
+        """ Loosens grip and backs away from dumbbell.
+        """
+        self.upper_arm()
+        self.lower_arm()
+        self.open_grip()
+        self.move_back()
+
+    # STATE UPDATE FUNCS
     def update_distance(self, data):
         self.distance = data.ranges[0]
     def update_image(self, data):
-        self.image    = data
+        self.image = data
 
     def run(self):
-        self.open_grip()
-
         while True:
-            self.lower_arm()
-            time.sleep(2)
-            self.raise_arm()
+            # self.open_grip()
+            self.open_grip()
+            while True:
+                self.pick_up()
+                time.sleep(3)
+                self.let_go()
+            # self.move_fwd()
+
         rospy.spin()
 
 if __name__ == "__main__":
